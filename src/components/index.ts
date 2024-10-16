acc72899471ad3760c05e4bb98138e678ebc971a
farfromrefug <>
2024-10-16 21:40:01 +0200

    fix: expose ios / android components (wrapping #if __ANDROID__/__IOS__)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Template } from "./Template.svelte";
 export { default as AsComponent } from "./AsComponent.svelte";
-// export { default as ios } from "./ios.svelte";
-// export { default as android } from "./android.svelte";
+export { default as ios } from "./ios.svelte";
+export { default as android } from "./android.svelte";
 
